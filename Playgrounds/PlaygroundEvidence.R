9995d7448ac8d56e8733f97d01275244efb61769
Will Beasley <wibeasley@hotmail.com>
2014-07-18 16:09:07 -0400

    initial playground complete

--- Playgrounds/PlaygroundEvidence.R
+++ Playgrounds/PlaygroundEvidence.R
@@ -1,17 +1,20 @@
 base::rm(list=base::ls(all=TRUE)) #Clear memory from previous runs.
+require(ggplot2)
+
 
 baseRate <- 0.3
 muN <- 35 #'N' stands for 'Nondiseased'
-sigmaN <- 10
+sigmaN <- 15
 muD <- 70 #'D' stands for 'Diseased'
-sigmaD <- 7
+sigmaD <- 20
 measurementRange <- c(0, 100)
+colorNondiseased <- "blue3"
+colorDiseased <- "red3"
 
 CalculateNondiseasePdf <- function( score, mu, sigma, baseRate=NA ) {
   baseRate <- ifelse(is.na(baseRate), 0, baseRate) #Notice missing values are set to 0, not 1 like with diseased.
   return( dnorm(x=score, mean=mu, sd=sigma) * (1-baseRate))
 }
-
 CalculateDiseasePdf <- function( score, mu, sigma, baseRate=NA ) {
   baseRate <- ifelse(is.na(baseRate), 1, baseRate) #Notice missing values are set to 1, not 0 like with nondiseased.
   return( dnorm(x=score, mean=mu, sd=sigma) * baseRate)
@@ -38,3 +41,24 @@ ds$LRInstant <- ds$DiseasedPdf / ds$NondiseasedPdf
 ds$LRPlus <- (ds$DiseasedCdfR)  / (ds$NondiseasedCdfR)
 ds$LRMinus <- (ds$DiseasedCdfL)  / (ds$NondiseasedCdfL)
 ds$LRRatio <- ds$LRPlus / ds$LRMinus
+
+FxToMinimize <- function( x ) {
+  difference <- (CalculateNondiseasePdf(score=x,  muN, sigmaN, baseRate=NA) - CalculateDiseasePdf(score=x, muD, sigmaD, baseRate=NA))
+  return( abs(difference) )
+}
+searchRange <- sort(c(muD, muN)) + c(-1, 1)
+# FxToMinimize(54)
+intersectX <- optimize(f=FxToMinimize, interval=c(30, 80) )$minimum
+intersectY <- CalculateNondiseasePdf(score=intersectX,  muN, sigmaN, baseRate=NA)
+peakN <- CalculateNondiseasePdf(score=muN,  muN, sigmaN, baseRate=NA)
+peakD <- CalculateDiseasePdf(score=muD,  muD, sigmaD, baseRate=NA)
+
+ggplot(ds, aes(x=ds$Score)) +
+  geom_line(aes(y=NondiseasedPdf), color=colorNondiseased, size=4, alpha=.5) +
+  geom_line(aes(y=DiseasedPdf), color=colorDiseased, size=4, alpha=.5) +
+  annotate(geom="segment", x=intersectX, y=intersectY, xend=intersectX, yend=0, size=4, alpha=.2, lineend="butt", color=colorNondiseased) +
+  annotate(geom="segment", x=intersectX, y=intersectY, xend=intersectX, yend=0, size=4, alpha=.2, lineend="butt", color=colorDiseased) +
+  annotate(geom="text", label="Nondiseased", x=muN, y=peakN, vjust=-.5, color=colorNondiseased) +
+  annotate(geom="text", label="Diseased", x=muD, y=peakD, vjust=-.5, color=colorDiseased) +
+  annotate(geom="text", label="Cutoff", x=intersectX, y=0, hjust=-.05, color="gray30", angle=90) +
+  theme_bw()
--- app/Run.R
+++ app/Run.R
@@ -3,5 +3,5 @@ rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is 
 library(shiny)
 # server <- (Sys.info()["nodename"] == "Lucky1304")
 # server <- FALSE
-shiny::runApp('ShinyApp/expansion', launch.browser=TRUE, port=6149)
+shiny::runApp('app', launch.browser=TRUE, port=6149)
 # To stop the Shiny server, click the red stop sign in the 'Console window.
