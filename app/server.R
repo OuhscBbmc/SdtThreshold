acac87fb8ac05d6f849ea4db7029d26c74772268
Will Beasley <wibeasley@hotmail.com>
2014-07-19 19:37:54 -0400

    Annotated Bayesian graph

--- app/server.R
+++ app/server.R
@@ -16,8 +16,8 @@ require(RColorBrewer)
 measurementRange <- c(-40, 100)
 stepWidthMeasurement <- 1
 stepWidthProbability <- .01
-paletteDisease <- c("T"="red3", "F"="blue3")
-paletteTest <- RColorBrewer::brewer.pal(n=2, name="Accent"); names(paletteTest) <- c("P", "N")
+paletteDisease <- RColorBrewer::brewer.pal(n=3, name="Set1")[1:2]; names(paletteDisease) <- c("T", "F")
+paletteTest <- RColorBrewer::brewer.pal(n=3, name="Accent")[2:3]; names(paletteTest) <- c("P", "N")
 #######################################
 ### Declare funcions that don't depend on reactives or user inputs.
 CalculateNondiseasePdf <- function( score, mu, sigma, baseRate=NA ) {
@@ -47,6 +47,12 @@ CalculateSpecificity <- function( scores, mu, sigma, baseRate=NA ) { #CalculateN
 }
 CalculateSensitivity <- function( scores, mu, sigma, baseRate=NA ) { #1 - CalculateDiseaseCdf
   return( 1 - CalculateDiseaseCdf(scores, mu, sigma, baseRate) )
+}
+CalculatePosteriorPositive <- function( prior, sensitivity, specificity ) {
+  return( (sensitivity * prior) / ((sensitivity * prior)+((1-specificity) * (1-prior))) )
+}
+CalculatePosteriorNegative <- function( prior, sensitivity, specificity ) {
+  return( ((1-sensitivity) * prior) / (((1-sensitivity) * prior)+(specificity * (1-prior))) )
 }
 #######################################
 ### Load and tweak data
@@ -91,8 +97,10 @@ shinyServer(function(input, output, session) {
     ds$UtilityNondiseased <- CalculateUtilityNondiseased(probability=ds$Probability, uTN=s$uTN, uFN=s$uFN)
     ds$UtilityDiseased <- CalculateUtilityDiseased(probability=ds$Probability, uFP=s$uFP, uTP=s$uTP)
     ds$Prior <- ds$Probability
-    ds$PosteriorPositive <- (SensitivityAtCutoff() * ds$Prior) / ((SensitivityAtCutoff() * ds$Prior)+((1-SpecificityAtCutoff()) * (1-ds$Prior)))
-    ds$PosteriorNegative <- ((1-SensitivityAtCutoff()) * ds$Prior) / (((1-SensitivityAtCutoff()) * ds$Prior)+(SpecificityAtCutoff() * (1-ds$Prior)))
+    # ds$PosteriorPositive <- (SensitivityAtCutoff() * ds$Prior) / ((SensitivityAtCutoff() * ds$Prior)+((1-SpecificityAtCutoff()) * (1-ds$Prior)))
+    # ds$PosteriorNegative <- ((1-SensitivityAtCutoff()) * ds$Prior) / (((1-SensitivityAtCutoff()) * ds$Prior)+(SpecificityAtCutoff() * (1-ds$Prior)))
+    ds$PosteriorPositive <- CalculatePosteriorPositive(ds$Prior, SensitivityAtCutoff(), SpecificityAtCutoff())
+    ds$PosteriorNegative <- CalculatePosteriorNegative(ds$Prior, SensitivityAtCutoff(), SpecificityAtCutoff())
     return( ds )
   })
   PdfDifference <- function( x ) {
@@ -112,7 +120,6 @@ shinyServer(function(input, output, session) {
   PdfIntersectY <- reactive({
     CalculateNondiseasePdf(score=PdfIntersectX(),  userInputs()$muN, userInputs()$sigmaN, baseRate=NA)
   })
-  
   SpecificityAtCutoff <- reactive({
     s <- userInputs()
     CalculateSpecificity( scores=PdfIntersectX(), mu=s$muN, sigma=s$sigmaN, baseRate=NA )
@@ -128,6 +135,7 @@ shinyServer(function(input, output, session) {
     return( difference )
   }
   ThresholdIntersectX <- reactive({
+    #TODO: calculate this as the intersection = (uTN-uFP)/(uTP-uFN); figure out how to respond when it's outside [0, 1].
     u <- NULL
     try(
       u <- uniroot(f=ThresholdDifference, interval=c(0,1)),
@@ -144,20 +152,57 @@ shinyServer(function(input, output, session) {
   peakD <- reactive({
     CalculateDiseasePdf(score=userInputs()$muD,  userInputs()$muD, userInputs()$sigmaD, baseRate=NA)
   })
-  
-  output$lblSpecificity <- reactive({ return( paste("Specificity at Cutoff: ", round(SpecificityAtCutoff(), 3)) ) })
-  output$lblSensitivity <- reactive({ return( paste("Sensitivity at Cutoff: ", round(SensitivityAtCutoff(), 3)) ) })
+  ThresholdPositiveTestDifference <- function( prior ) {
+    difference <- CalculatePosteriorPositive(prior, SensitivityAtCutoff(), SpecificityAtCutoff()) - ThresholdIntersectX()
+    return( difference )
+  }
+  ThresholdNegativeTestDifference <- function( prior ) {
+    difference <- CalculatePosteriorNegative(prior, SensitivityAtCutoff(), SpecificityAtCutoff()) - ThresholdIntersectX()
+    return( difference )
+  }
+  ThresholdPositiveTestIntersect <- reactive({
+    u <- NULL
+    try(
+      u <- uniroot(f=ThresholdPositiveTestDifference, interval=c(0,1)),
+      silent = TRUE
+    )
+    return( ifelse(!is.null(u), u$root, NA_real_) )  
+  })
+  ThresholdNegativeTestIntersect <- reactive({
+    u <- NULL
+    try(
+      u <- uniroot(f=ThresholdNegativeTestDifference, interval=c(0,1)),
+      silent = TRUE
+    )
+    return( ifelse(!is.null(u), u$root, NA_real_) )  
+  })
+  output$lblCutoff <- reactive({ return( paste("Cutoff:", round(PdfIntersectX())) ) })
+  output$lblSpecificity <- reactive({ return( paste("Specificity at Cutoff:", round(SpecificityAtCutoff(), 3)) ) })
+  output$lblSensitivity <- reactive({ return( paste("Sensitivity at Cutoff:", round(SensitivityAtCutoff(), 3)) ) })
+  output$lblTxThreshold <- reactive({ return( paste("Treatment Threshold:", round(ThresholdIntersectX(), 3)) ) })
   
   output$plotBayesian <- renderPlot({
+    thresholdPositiveTestIntersectX <- ThresholdPositiveTestIntersect()
+    thresholdPositiveTestIntersectY <- CalculatePosteriorPositive(thresholdPositiveTestIntersectX, SensitivityAtCutoff(), SpecificityAtCutoff())
+    thresholdNegativeTestIntersectX <- ThresholdNegativeTestIntersect()
+    thresholdNegativeTestIntersectY <- CalculatePosteriorNegative(thresholdNegativeTestIntersectX, SensitivityAtCutoff(), SpecificityAtCutoff())
+    
+    
     ds <- ProbabilityData()
     g <- ggplot(ds, aes(x=Prior)) +
       geom_path(aes(y=PosteriorPositive), color=paletteTest["P"], size=4, alpha=.5, lineend="round") +
       geom_path(aes(y=PosteriorNegative), color=paletteTest["N"], size=4, alpha=.5, lineend="round") +
+      geom_hline(yintercept=ThresholdIntersectX(), linetype="F3", color="gray30", alpha=.5)+
       annotate(geom="segment", x=0, y=0, xend=1, yend=1, size=2, alpha=.2, lineend="round") +
+      annotate(geom="segment", x=thresholdPositiveTestIntersectX, y=thresholdPositiveTestIntersectX, xend=thresholdPositiveTestIntersectX, yend=thresholdPositiveTestIntersectY, size=2, alpha=.2,lineend="round") +
+      annotate(geom="segment", x=thresholdNegativeTestIntersectX, y=thresholdNegativeTestIntersectX, xend=thresholdNegativeTestIntersectX, yend=thresholdNegativeTestIntersectY, size=2, alpha=.2, lineend="round") +
+      
       annotate(geom="text", label="probability given\npositive test", x=0, y=1, hjust=0, vjust=1, color=paletteTest["P"]) +
       annotate(geom="text", label="probability given\nnegative test", x=1, y=0, hjust=1, vjust=0, color=paletteTest["N"]) +
       annotate(geom="text", label="if no test", x=.5, y=.5, hjust=.5, angle=45, color="gray30", alpha=.5) +
-#       annotate(geom="text", label="Cutoff", x=PdfIntersectX(), y=0, hjust=-.05, color="gray30", angle=90) +
+      annotate(geom="text", label="tx\nthreshold", x=1, y=ThresholdIntersectX(), hjust=1, vjust=.5, color="gray30", alpha=.5) +
+      annotate(geom="text", label="NoTest/Test\nThreshold", x=thresholdPositiveTestIntersectX, y=thresholdPositiveTestIntersectX, hjust=0, vjust=.5, angle=90, color="gray30", alpha=.5) +
+      annotate(geom="text", label="Test/Treat\nThreshold", x=thresholdNegativeTestIntersectX, y=ThresholdIntersectX(), hjust=0, vjust=.5, angle=90, color="gray30", alpha=.5) +
       scale_x_continuous(label=scales::percent) +
       scale_y_continuous(label=scales::percent) +
       coord_fixed() +
--- app/ui.R
+++ app/ui.R
@@ -12,8 +12,10 @@ sliderWidth <- "100%"
 shinyUI(pageWithSidebar(
   headerPanel('Rob Hamm\'s SDT and Threshold Comparison'),
   sidebarPanel(
+    textOutput(outputId='lblCutoff'), #TODO: add a tooltip
     textOutput(outputId='lblSpecificity'), #TODO: add a tooltip with something like '(ie, the area of the blue curve left of the cutoff)'
     textOutput(outputId='lblSensitivity'), #TODO: add a tooltip with something like '(ie, the area of the red curve right of the cutoff)'
+    textOutput(outputId='lblTxThreshold'), #TODO: add a tooltip
     sliderInput(inputId="muN", label="Nondiseased Mean", min=0, max=100, value=0, step=1, width=sliderWidth),
     sliderInput(inputId="muD", label="Diseased Mean", min=0, max=100, value=25, step=1, width=sliderWidth),
     sliderInput(inputId="sigmaN", label="Nondiseased SD", min=5, max=40, value=10, step=1, width=sliderWidth),
@@ -21,12 +23,12 @@ shinyUI(pageWithSidebar(
     sliderInput(inputId="uTP", label="u(TP) = Utility of a True, Positive Decision to Treat", min=0, max=1, value=.90, step=.01, width=sliderWidth),
     sliderInput(inputId="uFN", label="u(FN) = Utility of a False, Negative Decision to Treat", min=0, max=1, value=.80, step=.01, width=sliderWidth),
     sliderInput(inputId="uFP", label="u(FP) = Utility of a False, Positive Decision to Treat", min=0, max=1, value=.95, step=.01, width=sliderWidth),
-    sliderInput(inputId="uTN", label="u(TN) = Utility of a True, Negative Decision to Treat", min=0, max=1, value=.99, step=.01, width=sliderWidth)    
+    sliderInput(inputId="uTN", label="u(TN) = Utility of a True, Negative Decision to Treat", min=0, max=1, value=.99, step=.01, width=sliderWidth),
+    "Robert M. Hamm, PhD; Clinical Decision Making Program; Department of Family Medicine; University of Oklahoma Health Sciences Center; Oklahoma City OK 73190; 405/271-8000  ext 32306; Fax:  405/271-4125; email:  robert-hamm@ouhsc.edu"
   ),
   mainPanel(
     plotOutput('plotBayesian'),
-    plotOutput('plotPdf'),
-    plotOutput('plotTxThreshold'),
+    plotOutput('plotPdf'),    plotOutput('plotTxThreshold'),
     plotOutput('plotRoc')
   )
 ))
